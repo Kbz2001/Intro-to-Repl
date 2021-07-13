60fa75890b06131b27240f23b0f2b53c22381d9e
Kbz <kilezisme@gmail.com>
2021-07-13 00:52:08 +0000

    Python Programming: Intro Finished Day 1

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print("Hello World")
+#
+
+# print("Hello")
+# fullName = input("What is your first and last name? ")
+# age = input("How old are you?   ")
+# favorite_activity = input("What is your favorite thing to do? ")
+
+# Concatination
+# print("Your name is " + fullName)
+# print("You are ", age, " years old")
+# print("Your favorite activity is " + favorite_activity)
+
+# -----------------------------------------------------
+# Data Type Conversion
+var1 = 1 # integer value
+var_string = str(var1)
+# print(var1)
+# print(var_string)
+# int() (converts to an integer)
+# float() (or a double)
+# str() (converts to a string)
+
+#-----------------------------------
+# Arithmetic Operators
+# Addition +
+# Subtraction -
+# Multiplication *
+# Division /
+
+#-------------------------------
+# Get perimeter of a rectangle
+# User inputs length and width
+# console outputs perimeter
+
+# String manipulation
+# Concatination -> "Hello" "World" -> "Hello World"
+# length -> "hello" -> len("hello") -> 5
+# upper -> Convert all characters in a string to uppercase. "hello" -> "HELLO"
+# lower -> Converts all characters in a string to lowercase "HELLO" -> "hello"
+
+# string1 = "hello"
+# string2 = "HELLO"
+
+# print(string1.upper())
+# print(string2.lower())
+# print(len(string1))
+# print(string1 + string2)
+
+# String formatting
+# name = input("Enter name: ")
+# number = int(input("Enter number"))
+# print(f"Your name is {name} and your favorite number is {number}")
+# # print("Your name is" , name + "and your favorite number is ", number)
+# name2 = "Mr. Z"
+# age2 = "20"
+# print("My name is {0}, I am {1}".format(name2,age2))
+
+# Heroes Mad Libs Project
+print(input("Hello, what is your name?"))
